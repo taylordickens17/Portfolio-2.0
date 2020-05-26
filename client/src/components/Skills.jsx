2d81a56981123d9027685e11b2534f69c63f173e
TaylorDickens <justindickens17@gmail.com>
2020-05-26 16:31:09 -0400

    made some more changes to the skill section

--- client/src/components/Skills.jsx
+++ client/src/components/Skills.jsx
@@ -11,6 +11,42 @@ const Skills = () => {
             <Container>
                 <h1 className="skills-title">Skills</h1>
                 <Row>
+                    <Col md="2" className="skills-col">
+                        <Card className="project-card-pets">
+                            <FontAwesomeIcon className="skill-icon-node" size="4x" icon={faNodeJs} />
+                            <a href="">NodeJS</a>
+                        </Card>
+                    </Col>
+                    <Col md="2" className="skills-col">
+                        <Card className="project-card-space">
+                            <FontAwesomeIcon className="skill-icon-react" size="4x" icon={faReact} />
+                            <a href="">React</a>
+                        </Card>
+                    </Col>
+                    <Col md="2" className="skills-col">
+                        <Card className="project-card-book">
+                            <FontAwesomeIcon className="skill-icon-docker" size="4x" icon={faDocker} />
+                            <a href="">Docker</a>
+                        </Card>
+                    </Col>
+                    <Col md="2" className="skills-col">
+                        <Card className="project-card-pets">
+                            <FontAwesomeIcon className="skill-icon-heroku" size="4x" icon={faCloudUploadAlt} />
+                            <a href="">Heroku</a>
+                        </Card>
+                    </Col>
+                    <Col md="2" className="skills-col">
+                        <Card className="project-card-space">
+                            <FontAwesomeIcon className="skill-icon-adobe" size="4x" icon={faAdobe} />
+                            <a href="">Adobe</a>
+                        </Card>
+                    </Col>
+                    <Col md="2" className="skills-col">
+                        <Card className="project-card-book">
+                            <FontAwesomeIcon className="skill-icon-book" size="4x" icon={faFigma} />
+                            <a href="">Figma</a>
+                        </Card>
+                    </Col>
                 </Row>
             </Container>
         </div>
